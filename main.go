d7e9afab0b32711aacb78f886a27ea9591a6f021
Mujahid <mujahid.170997@gmail.com>
2023-04-17 09:55:51 +0700

    Init project

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/mujahxd/eventgenie/config"
+	"github.com/mujahxd/eventgenie/utils/database"
+)
+
+func main() {
+	e := echo.New()
+	loadConfig := config.InitConfig()
+	db := database.ConnectionDB(loadConfig)
+
+	// database
+	database.Migrate(db)
+
+	e.Logger.Fatal(e.Start(":8000"))
+}
