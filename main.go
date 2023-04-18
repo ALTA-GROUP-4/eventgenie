94933d3705abfee1adc591d479f83bc1ca5b70b1
Mujahid <mujahid.170997@gmail.com>
2023-04-18 10:44:42 +0700

    add register user routes

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	userhandler "github.com/mujahxd/eventgenie/app/features/user/handler"
+	userrepo "github.com/mujahxd/eventgenie/app/features/user/repository"
+	userusecase "github.com/mujahxd/eventgenie/app/features/user/usecase"
 	"github.com/mujahxd/eventgenie/config"
+	"github.com/mujahxd/eventgenie/routes"
 	"github.com/mujahxd/eventgenie/utils/database"
 )
 
@@ -13,6 +17,13 @@ func main() {
 
 	// database
 	database.Migrate(db)
+	userRepository := userrepo.NewRepository(db)
+
+	userService := userusecase.NewService(userRepository)
+
+	userHandler := userhandler.NewHandler(userService)
+
+	routes.UserRoute(e, userHandler)
 
 	e.Logger.Fatal(e.Start(":8000"))
 }
--- routes/user_routes.go
+++ routes/user_routes.go
@@ -1,1 +1,15 @@
 package routes
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/mujahxd/eventgenie/app/features/user"
+)
+
+func UserRoute(e *echo.Echo, h user.UserHandler) {
+	// e.Pre(middleware.RemoveTrailingSlash())
+	// e.Use(middleware.CORS())
+	// e.Use(middleware.Logger())
+
+	e.POST("/users", h.RegisterUser())
+
+}
